4506cde8e1a8465c77dc0221fa4f1d2a33771ca2
fabio <fabiohmaru@gmail.com>
2022-05-23 22:50:42 +0100

    Tocando FH

--- src/app/pokedex/poke-collapse/poke-collapse.module.ts
+++ src/app/pokedex/poke-collapse/poke-collapse.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
--- src/app/pokedex/poke-collapse/poke-collapse.page.ts
+++ src/app/pokedex/poke-collapse/poke-collapse.page.ts
@@ -1,4 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {PokedexService} from '../pokedex.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Pokemon} from '../pokemon';
 
 @Component({
   selector: 'app-poke-collapse',
@@ -6,8 +9,27 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./poke-collapse.page.scss'],
 })
 export class PokeCollapsePage implements OnInit {
+  public pokedex: Pokemon[] | undefined;
+  @Output() selected = new EventEmitter<string>();
+
+  constructor(private pokedexService: PokedexService){}
 
   ngOnInit() {
+    this.getPokedex();
   }
 
+  public getPokedex(): void {
+    this.pokedexService.getPokedex().subscribe(
+      (response: Pokemon[]) => {
+        this.pokedex = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public changeNames(name: string) {
+    this.selected.emit(name);
+  }
 }
